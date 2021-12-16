08e624d26dc19c6726d23138870455338628404b
Konteh <kontehebrima993@gmail.com>
2021-12-15 20:30:02 -0800

    active the clear button and the color select button

--- main.js
+++ main.js
@@ -11,13 +11,25 @@ ctx.lineWidth = 5
 
 let draw = false
 
-// Set draw to true when mouse is pressed
+let clrs = document.querySelectorAll(".clr")
+clrs = Array.from(clrs)
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.dataset.clr
+    })
+})
+
+let clearBtn = document.querySelector(".clear")
+clearBtn.addEventListener("click", () => {
+    // Clearning the entire canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+
 window.addEventListener("mousedown", (e) => draw = true)
-// Set draw to false when mouse is released
 window.addEventListener("mouseup", (e) => draw = false)
 
 window.addEventListener("mousemove", (e) => {
-    // if draw is false then we won't draw
     if(prevX == null || prevY == null || !draw){
         prevX = e.clientX
         prevY = e.clientY
