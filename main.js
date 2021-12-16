73370a551e1b99a93725337553cae8087158755b
Konteh <kontehebrima993@gmail.com>
2021-12-15 20:10:28 -0800

    enable drawing on screen

--- main.js
+++ main.js
@@ -4,7 +4,34 @@ canvas.width = window.innerWidth
 
 const ctx = canvas.getContext("2d")
 
+let prevX = null
+let prevY = null
+
+ctx.lineWidth = 5
+
+let draw = false
+
+// Set draw to true when mouse is pressed
+window.addEventListener("mousedown", (e) => draw = true)
+// Set draw to false when mouse is released
+window.addEventListener("mouseup", (e) => draw = false)
+
 window.addEventListener("mousemove", (e) => {
-    console.log("Mouse X: " + e.clientX)
-    console.log("Mouse Y: " + e.clientY)
+    // if draw is false then we won't draw
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
+
+    let currentX = e.clientX
+    let currentY = e.clientY
+
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+
+    prevX = currentX
+    prevY = currentY
 })
